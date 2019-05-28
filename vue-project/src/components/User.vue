81bbd84ef19b17fe482f6cce4dbf70c43d705693
Vinicius Lelis <vinicius.ribeiro20@fatec.sp.gov.br>
2019-05-28 11:49:04 -0300

    Add files via upload

--- vue-project/src/components/User.vue
+++ vue-project/src/components/User.vue
@@ -24,22 +24,104 @@
         </div>
         
         <v-timeline>
-            <v-timeline-item v-for="n in 4" :key="n" color="orange lighten-3" large>
+            <v-timeline-item  v-for="produto in sortedArray" :key="produto.id" color="orange lighten-3" large>
                 <v-card class="elevation-6">
-                    <v-card-title class="headline">Nome do produto</v-card-title>
+                    <v-card-title class="headline">{{ produto.nome }}</v-card-title>
                     <v-card-text>
-                        Data de validade:
+                        Validade: {{ produto.periodo_validade }} dias
                         <br>
-                        Fornecedor:
+                        Fornecedor: {{ produto.fornecedor }}
                     </v-card-text>
                 </v-card>
             </v-timeline-item>
+            
         </v-timeline>
+
+                 
                     
     </div>
+
+    
 
 </template>
 
 <script>
+import axios from 'axios'
+import { mapState } from 'vuex'
+import Produto from './Produto'
 
+export default {
+  //name: 'produtos',
+  components: {
+      Produto
+    },
+  data() {
+    return {
+      header: {
+                'Access-Control-Allow-Origin': 'http://localhost:8080',
+                'Token': 'Baerer '+this.token
+            },
+      assunto: '',
+      texto: '',
+      anotacoes: [],
+      produtos:[],
+      busca: '',
+      dialog: false,
+      alert: false,
+      produto: null,
+    }
+  },
+  computed: {
+    ...mapState([
+      'usuario'
+    ])
+  },
+  methods: {
+  
+    buscar(){
+      console.log(this.token);
+      if(this.busca != ""){
+        axios.get('springRest/api/busca/'+this.busca, 
+          this.header )
+        .then(res => {
+          console.log(res)
+          this.produtos = res.data
+        })
+        .catch(error => console.log(error))
+      }else{
+        this.atualizar();
+      }
+    },    
+    atualizar () {
+      axios.get('springRest/api/produtos', 
+          { headers: { Accept: 'application/json' } })
+        .then(res => {
+          console.log(res)
+          this.produtos = res.data
+        })
+        .catch(error => console.log(error))
+    },
+   },  
+  created () {
+    this.atualizar()
+  },
+  computed:{
+        token:{
+            get(){
+                return this.$store.state.token;
+            }
+        },
+        sortedArray: function() {
+            function compare(a, b) {
+            if (a.periodo_validade < b.periodo_validade)
+                return -1;
+            if (a.periodo_validade > b.periodo_validade)
+                return 1;
+            return 0;
+      }
+
+      return this.produtos.sort(compare);
+    }
+    }
+}
 </script>
